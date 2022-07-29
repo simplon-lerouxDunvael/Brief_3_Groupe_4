7256a5b0931b3aa055e3b2c452ac666bd2968f54
Nicolas MARTY <108000701+simplon-nicolasmarty@users.noreply.github.com>
2022-07-29 12:30:41 +0200

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,5 +1,30 @@
 sudo su - 
 apt-get update 
+
+
+### Installation LVM
+apt-get install lvm2 && /etc/init.d/lvm start
+
+### Création Volume Physique
+pvcreate /dev/sdc 
+
+vgcreate monGV-GR4 /dev/sdc  
+
+### Créer un Volume logique dans un VG
+lvcreate -n monLV-GR4-LOG -L 20g monGV-GR4 
+lvcreate -n monLV-GR4-USERS -L 30g monGV-GR4 
+
+### Formater le disque logique dans le système de fichiers souhaité 
+mkfs -t ext4 /dev/monGV-GR4/monVL-GR4-LOG
+mkfs -t ext4 /dev/monGV-GR4/monVL-GR4-USERS
+
+mv /var/log /var/log2 
+
+mount /dev/monGV-GR4/monVL-GR4-LOG /var/log
+mount /dev/monGV-GR4/monVL-GR4-USERS /var/lib/jenkins/userContent
+
+mv -f /var/log2 /var/log 
+
 apt-get install -y openjdk-11-jdk 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee \  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \  https://pkg.jenkins.io/debian-stable binary/ | tee \  /etc/apt/sources.list.d/jenkins.list > /dev/null
